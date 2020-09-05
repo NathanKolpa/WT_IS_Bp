30b369fdee45e580e1c9769bc7ae098cc3d5df19
Fritz van Deventer <fritzvd@users.noreply.github.com>
2020-09-05 13:38:25 +0200

    myimdb import (#30)
    Ik was even bezig dit uit te proberen en wilde wat dingen herschrijven zodat de MYIMDB database gedownload en geimporteerd kon worden. (zie [de file FLETNIX_DOCENT.zip](https://onderwijsonline.han.nl/elearning/lessonfile/dyAY421y/eyJpdiI6IjdGeTBlTHM0M0kralhpcEF5TzYzQUE9PSIsInZhbHVlIjoiZDBtQ3phQzhiRFlKUFpVbDVIZW9HdDU2bVdlZFo3Z3Q2RVwvbmlSVCtVYjA9IiwibWFjIjoiZGQ2ODlhZDE0ZTBjZWM1Y2EyODkwN2ExODE4OWUwNDNhYzYxNGRjYWIwMjlhYWE5N2Y2YWRlM2M0MDQzMzNkYyJ9))
    
    Alleen ik kom er niet uit. Ik weet niet zo goed waar het door komt. Het lijkt al mis te gaan bij het wachtwoord. Maar ik krijg de error niet zo goed boven water in de huidige setup. Zin om mee te kijken?
    
    https://github.com/HANICA/WT_IS_Bp/pull/30

--- webserver/applicatie/src/data/db_lezen.php
+++ webserver/applicatie/src/data/db_lezen.php
@@ -1,21 +1,19 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types = 1);
 
 namespace fletnix\data;
 
 use fletnix\config\Db;
-use function fletnix\data\printPdoError;
 use PDO;
-use PDOException;
 
 require_once __DIR__ . '/../../config/bootstrap.php';
 require_once ROOT_DIR . '/src/data/db_verbinden.php';
 
 function leesDb()
 {
-    $buffer = "<h1>Vendors</h1>";
-    $query = "SELECT TOP (100) [Name] FROM Purchasing.Vendor ORDER BY [Name];";
+    $buffer = "<h1>Movies</h1>";
+    $query = "SELECT TOP (100) * FROM Movie WHERE [title] LIKE '%pink panther%' ORDER BY [publication_year];";
     $password = rtrim(file_get_contents('/run/secrets/password_rdbms_app', true));
     if (!$password) {
         throw new RuntimeException("Kon SA's wachtwoord (SQL Server) niet uitlezen. ");
@@ -26,17 +24,13 @@ function leesDb()
         unset($password);
     }
     $pdostatement = $verbinding->prepare($query);
-    try {
-        if (!$pdostatement->execute()) {
-            printPdoError($pdostatement);
-            throw new RuntimeException("Uitvoering PDO-statement mislukt. ", 0);
-        }
-    } catch (PDOException $fout) {
-        printPdoError($pdostatement);
-        throw new RuntimeException("Kon PDO-statement niet uitvoeren. ", 0, $fout);
+    if (!$pdostatement->execute()) {
+        throw new RuntimeException("Uitvoering PDO-statement mislukt. ", 0);
     }
     while ($rij = $pdostatement->fetch(PDO::FETCH_LAZY)) {
-        $buffer .= htmlentities($rij['Name']);
+        $buffer .= htmlentities($rij['publication_year']);
+        $buffer .= ' - ';
+        $buffer .= htmlentities($rij['title']);
         $buffer .= "<br/>";
     }
     unset($pdostatement);
