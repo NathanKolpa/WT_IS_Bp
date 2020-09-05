d8bfbe39c9785543ceef0189519e4ee7ded62e7e
Fritz van Deventer <fritzvd@users.noreply.github.com>
2020-09-05 13:38:25 +0200

    myimdb import (#30)
    Ik was even bezig dit uit te proberen en wilde wat dingen herschrijven zodat de MYIMDB database gedownload en geimporteerd kon worden. (zie [de file FLETNIX_DOCENT.zip](https://onderwijsonline.han.nl/elearning/lessonfile/dyAY421y/eyJpdiI6IjdGeTBlTHM0M0kralhpcEF5TzYzQUE9PSIsInZhbHVlIjoiZDBtQ3phQzhiRFlKUFpVbDVIZW9HdDU2bVdlZFo3Z3Q2RVwvbmlSVCtVYjA9IiwibWFjIjoiZGQ2ODlhZDE0ZTBjZWM1Y2EyODkwN2ExODE4OWUwNDNhYzYxNGRjYWIwMjlhYWE5N2Y2YWRlM2M0MDQzMzNkYyJ9))
    
    Alleen ik kom er niet uit. Ik weet niet zo goed waar het door komt. Het lijkt al mis te gaan bij het wachtwoord. Maar ik krijg de error niet zo goed boven water in de huidige setup. Zin om mee te kijken?
    
    https://github.com/HANICA/WT_IS_Bp/pull/30

--- webserver/applicatie/src/data/db_verbinden.php
+++ webserver/applicatie/src/data/db_verbinden.php
@@ -1,26 +1,19 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types = 1);
 
 namespace fletnix\data;
 
 use fletnix\config\Db;
 use PDO;
-use PDOException;
 use RuntimeException;
 
 require_once ROOT_DIR . '/config/db.php';
 
 function verbindDb(string $databaseNaam, string $login, string $password)
 {
-    try
-    {
-        $verbinding = new PDO("sqlsrv:server=" . Db::HOST . "; Database=$databaseNaam", $login, $password);
-        $verbinding->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $fout) {
-        throw new RuntimeException("Kon geen verbinding maken met RDBMS. ", 0, $fout);
-    } finally {
-        unset($password);
-    }
+    $verbinding = new PDO("sqlsrv:server=" . Db::HOST . "; Database=$databaseNaam", $login, $password);
+    $verbinding->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    unset($password);
     return $verbinding;
 }
