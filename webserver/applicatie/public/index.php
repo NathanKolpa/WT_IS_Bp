9c62f4c1777ce476d8ca36560c09a39ab44172af
Sander Maijers <3374183+sanmai-NL@users.noreply.github.com>
2020-12-19 11:08:34 +0100

    Ondersteun forms insturen en bestanden serveren
    Verder: technische verbeteringen waardoor het opbouwen van de dev
    container voorspelbaarder en minder afhankelijk is van de
    internetverbinding.

--- webserver/applicatie/public/index.php
+++ webserver/applicatie/public/index.php
@@ -7,10 +7,16 @@ namespace fletnix\index;
 require_once 'config/bootstrap.php';
 require_once 'config/db.php';
 
-switch ($_SERVER['REQUEST_URI']) {
+/*
+`$_SERVER['REQUEST_URI']` is de volledige URL die binnenkomt op de webserver bij het request, vanaf het pad.
+Dus bij 'http://localhost/pad/naar/pagina?naam=Piet' is het `'/pad/naar/pagina?naam=Piet'`.
+Met `parse_url` zoals hieronder aangeroepen pak je alleen het pad-gedeelte.
+Dus dan wordt het `'/pad/naar/pagina'`.
+*/
+$urlPad = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($urlPad) {
   case '/':
-    require_once 'src/views/index.php';
-        break;
   case '':
     require_once 'src/views/index.php';
         break;
@@ -20,8 +26,34 @@ switch ($_SERVER['REQUEST_URI']) {
   case '/test_db':
     require_once 'src/views/test_db.php';
         break;
+  case '/verwerk_form_get':
+    require_once 'src/views/form_test_get.php';
+        break;
+  case '/verwerk_form_post':
+    require_once 'src/views/form_test_post.php';
+        break;
   default:
-    http_response_code(404);
-    require_once 'src/views/404.php';
+    /*
+    Er is geen pagina opgevraagd in het HTTP-request.
+    Ga ervan uit dat een bestand (zoals een stylesheet) is opgevraagd.
+    */
+    $isBestand = preg_match(
+      /*
+      Als het pad eindigt met `.css` of één van de andere door `|` gescheiden bestandsnaamextensies, is een geldig bestandstype opgevraagd.
+      */
+        '/\.(?:css|png|jpg|jpeg|svg|woff|woff2|ttf|otf|html)$/',
+        $urlPad
+    );
+    if ($isBestand) {
+      /*
+      Geef PHP het signaal dat geen pagina maar een bestand opgevraagd is.
+      PHP stuurt dan het bestand op naar de client.
+      */
+      return false;
+    } else {
+      // Het is een bekende pagina noch een geldig bestandstype.
+      http_response_code(404);
+      require_once 'src/views/404.php';
+    }
         break;
 }
--- webserver/provision.sh
+++ webserver/provision.sh
@@ -1,67 +1,33 @@
 #!/bin/sh
-
 set -eux
-
-sed -i -e 's/v[[:digit:]]\..*\//edge\//g' '/etc/apk/repositories'
-printf '%s\n' 'http://dl-cdn.alpinelinux.org/alpine/edge/testing' >>/etc/apk/repositories
-apk update
-apk add --no-cache 'git' 'nodejs-current' 'npm'
+cd -- "${TMPDIR:-/tmp/}/vendor/apks/"
+apk add --repositories-file='/dev/null' --allow-untrusted --no-cache --no-network -- *.apk
+cd ..
+# Installeer SQL Server ODBC-drivers en tools (nodig voor de sqlsrv driver).
+yes | apk add --repositories-file=/dev/null --allow-untrusted --no-network --no-cache 'msodbcsql17_17.6.1.1-1_amd64.apk'
+yes | apk add --repositories-file=/dev/null --allow-untrusted --no-network --no-cache 'mssql-tools_17.6.1.1-1_amd64.apk'
+# Installeer NodeJS-afhankelijke tools
 npm install -g 'npm'
 npm install -g 'hint' 'prettier' 'stylelint' 'stylelint-config-standard' 'stylelint-no-unsupported-browser-features'
-
-cd -- "${TMPDIR:-/tmp/}/vendor/"
-
+# Installeer PHP-tools
 mv 'php-cs-fixer.phar' '/usr/local/bin/php-cs-fixer'
 mv 'phpcbf.phar' '/usr/local/bin/phpcbf'
 mv 'phpcs.phar' '/usr/local/bin/phpcs'
 mv 'phpmd.phar' '/usr/local/bin/phpmd'
 mv 'phpstan.phar' '/usr/local/bin/phpstan'
-
-# Install SQL Server ODBC drivers and tools (required for the sqlsrv driver).
-yes | apk add --allow-untrusted 'msodbcsql17_17.6.1.1-1_amd64.apk'
-yes | apk add --allow-untrusted 'mssql-tools_17.6.1.1-1_amd64.apk'
-
-cd -
-
-apk add --no-cache 'php7' 'php7-dev' 'php7-pear' 'php7-pecl-xdebug' 'php7-pdo' 'php7-openssl' 'autoconf' 'make' 'g++' 'unixodbc-dev' 'openjdk11-jre-headless' 'shellcheck'
-
 php -i
-cp '/usr/local/etc/php/php.ini-development' '/usr/local/etc/php/php.ini'
+cd /root/
+cp 'php.ini' '/usr/local/etc/php/'
 pecl config-set php_ini '/usr/local/etc/php/php.ini'
-
-# Procedural sqlsrv. Enable for debugging only.
+cp 'docker-php-ext-xdebug.ini' 'security.ini' '/usr/local/etc/php/conf.d/'
+cd "${TMPDIR:-/tmp/}/vendor/"
+# Procedurele sqlsrv. Zet alleen aan voor debugging.
 #pecl install 'sqlsrv-5.9.0beta2'
 #docker-php-ext-enable 'sqlsrv'
-
-cd -- "${TMPDIR:-/tmp/}"'/vendor/pdo_sqlsrv-5.9.0beta2/pdo_sqlsrv-5.9.0beta2/'
+cd 'pdo_sqlsrv-5.9.0beta2/pdo_sqlsrv-5.9.0beta2/'
 phpize
 ./configure
 make
 make install
 cd -
-
 docker-php-ext-enable 'pdo_sqlsrv'
-
-printf '%s\n' 'file_uploads = Off
-allow_url_fopen = Off
-
-[Session]
-session.use_strict_mode = On
-session.use_cookies = On
-session.use_only_cookies = On
-session.cache_limiter = nocache
-session.cookie_httponly = On
-session.cookie_samesite = "Strict"
-session.use_trans_sid = Off
-' >>/usr/local/etc/php/conf.d/security.ini
-
-printf '%s\n' '[XDebug]
-xdebug.remote_enable = 1
-xdebug.remote_autostart = 1
-# Required to extend beyond localhost, because of Docker networking.
-xdebug.remote_host = "0.0.0.0"
-' >>/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-
-printf '%s\n' '[PHP]
-include_path = "/srv/webapplicatie/applicatie/:applicatie/"
-' >/usr/local/etc/php/conf.d/webapplicatie.ini
