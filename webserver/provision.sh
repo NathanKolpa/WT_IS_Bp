e0dcd01b89e8a7e95ee8f4727405e667834dacb4
Fritz van Deventer <fritzvd@users.noreply.github.com>
2021-10-07 10:14:35 +0200

    added git to provision

--- webserver/provision.sh
+++ webserver/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-apt install curl apt-transport-https wget lsb-release gnupg2 -y
+apt install curl apt-transport-https wget git lsb-release gnupg2 -y
 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
